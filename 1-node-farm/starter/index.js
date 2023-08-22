b2258e204f22f009d64f915311aee7cfb40c6301
campusricardo <campusricardo2023@gmail.com>
2023-08-22 13:27:46 -0500

    Fixed an error

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -37,6 +37,23 @@ const url = require('url');
 
 // SERVER
 
+const replaceTemplate = (temp, product ) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 
 const dataJson = JSON.parse(data)
@@ -44,11 +61,17 @@ const dataJson = JSON.parse(data)
 
 const server = http.createServer((req, res) => {
     const pathName = req.url;
-    
+    // Overview page
     if (pathName === '/' || pathName === '/overview' ) {
-        res.end('This is the OVERVIEW');
+        res.writeHead(200, {'Content-type': 'text/html'});
+
+        const cardsHTML = dataJson.map((e)=> replaceTemplate(tempCard, e)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHTML);
+        res.end(output);
+        // Product page
     } else if (pathName === '/product'){
         res.end('This is the PRODUCT');
+        // API
     } else if (pathName === '/api'){
 
         res.writeHead(200, {'Content-type': 'application/json'});
@@ -56,7 +79,7 @@ const server = http.createServer((req, res) => {
 
     }
     
-
+// Not Found
     else {
         res.writeHead(404, {
             'Content-type': 'text/html',
